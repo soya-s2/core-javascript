e8020f31756526f43b0cea684eba217070d0436c
soya-s2 <soya.s2.dev@gmail.com>
2021-05-31 22:11:56 +0900

    Revise comment

--- 04 콜백 함수/p.112-4.17.js
+++ 04 콜백 함수/p.112-4.17.js
@@ -23,8 +23,8 @@ var coffeeMaker = async function () {
   };
 coffeeMaker();
 
-/* ES2017의 async/await */
-/* 비동기 작업을 수행하고자 하는 함수 앞에 async를 표기
+/* ES2017의 async/await 
+비동기 작업을 수행하고자 하는 함수 앞에 async를 표기
 함수 내부에서 실질적 비동기 작업이 필요한 위치마다 await 표기
 뒤의 내용을 Promise로 자동 전환
 해당 내용이 resolve된 이후에야 다음으로 진행 */
